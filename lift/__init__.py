861653a1acb115b987654ff2dce718a65a162d64
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-19 22:47:25 +0200

    Update __init__.py

--- lift/__init__.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-
-from lift import gradient
-
-bot = commands.Bot(command_prefix="?", help_command=None, self_bot=True)
-
-def login(token):
-    bot.run(token=f"{token}", log_handler=None)
